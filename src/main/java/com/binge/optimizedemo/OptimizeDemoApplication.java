52b619f1a885e2243d2ff551efcd8861d253d213
Abin <abin@AbindeMacBook-Pro.local>
2020-08-06 00:14:27 +0800

    optimize-demo init

---
+++ src/main/java/com/binge/optimizedemo/OptimizeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.binge.optimizedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizeDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptimizeDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/binge/optimizedemo/OptimizeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binge.optimizedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizeDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
